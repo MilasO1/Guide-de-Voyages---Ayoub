7da9d9f81f9e71cabef0577143d22328b606122c
MilasO1 <milas.denim@proton.me>
2024-12-17 23:15:51 +0100

    Enfin ca marche

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ document.getElementById('toggleMap').addEventListener('click', () => {
 });
 
 
+
 // Derouler le carousel avec les fléches gauche et droite
 document.addEventListener("keydown", function(event) {
     const carousel = document.querySelector("#mainCarousel");
@@ -31,13 +32,10 @@ document.addEventListener("keydown", function(event) {
     }
 });
 
-
-
 // Autre methode en utilisant bootstrap.getInstance
 
 // document.addEventListener("keydown", function(event) {
 //     const carousel = document.querySelector("#mainCarousel");
-//     if (!carousel) return;
 
 //     const carouselInstance = bootstrap.Carousel.getInstance(carousel);
 
@@ -47,3 +45,51 @@ document.addEventListener("keydown", function(event) {
 //         carouselInstance.next();
 //     }
 // });
+
+// Ajouter des pointeurs pour chaque ville, avec lien vers la page Destinations
+function villesPointeur() {
+    const villes = [
+        { name: 'Paris', coords: [48.8566, 2.3522], description: 'Découvrez la Tour Eiffel, le Louvre, et bien plus encore !' },
+        { name: 'Rome', coords: [41.9028, 12.4964], description: '  Découvrez le Colisée, le Vatican, et bien plus encore ! ' },
+        { name: 'Barcelone', coords: [41.3851, 2.1734], description: 'Découvrez la Sagrada Família, le Parc Güell, et bien plus encore !' },
+        { name: 'Casablanca', coords: [33.5731, -7.5898], description: ' Admirez la Mosquée Hassan II, la Corniche, et bien plus encore !' },
+        { name: 'Dublin', coords: [53.3498, -6.2603], description: 'Explorez le Trinity College, le Guinness Storehouse, et bien plus encore !' },
+    ];
+
+    villes.forEach(dest => {
+        const marker = L.marker(dest.coords).addTo(map);
+        marker.bindPopup(`
+            <div class="map-popup">
+                <h3>${dest.name}</h3>
+                <p>${dest.description}</p>
+                <a href="destination.html">Lire plus...</a>
+            </div>
+        `);
+    });
+}
+
+villesPointeur();
+
+
+// Validation formulaire classique
+function submitForm(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const suggestion = document.getElementById('suggestion').value;
+    const message = document.getElementById('formMessage');
+
+    if (name === '' || email === '' || suggestion === '') {
+        message.innerHTML = '<div class="alert alert-danger">Tous les champs doivent être remplis.</div>';
+        return;
+    }
+
+    setTimeout(function() {
+        document.getElementById('suggestionForm').reset();
+
+        message.innerHTML = `<div class="alert alert-success">Merci pour votre suggestion, ${name} !</div>`;
+    }, 1000); 
+}
+
+
